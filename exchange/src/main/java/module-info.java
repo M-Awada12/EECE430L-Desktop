d02fdd8d1efd3a5dcdf846874e22be7745365367
M-Awada <m.awada1212@gmail.com>
2023-03-03 11:30:40 +0200

    deltet all

--- exchange/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mohammadawada.exchange {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.mohammadawada.exchange to javafx.fxml;
-    exports com.mohammadawada.exchange;
-}
